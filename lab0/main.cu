70efdf94bd5124916329f663e68b38f1277b89bf
Lorne_0 <alvin1994alvin@gmail.com>
2018-03-01 13:46:49 +0800

    lab0 draw a picture

---
+++ lab0/main.cu
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include "SyncedMemory.h"
+
+#define CHECK {\
+	auto e = cudaDeviceSynchronize();\
+	if (e != cudaSuccess) {\
+		printf("At " __FILE__ ":%d, %s\n", __LINE__, cudaGetErrorString(e));\
+		abort();\
+	}\
+}
+
+const int W = 40;
+const int H = 12;
+
+__global__ void Draw(char *frame) {
+	// TODO: draw more complex things here
+	// Do not just submit the original file provided by the TA!
+	const int y = blockIdx.y * blockDim.y + threadIdx.y;
+	const int x = blockIdx.x * blockDim.x + threadIdx.x;
+	if (y < H and x < W) {
+		char c;
+		if (x == W-1) {
+			c = y == H-1 ? '\0' : '\n';
+		} else if (y == 0 or y == H-1 or x == 0 or x == W-2) {
+			c = ':';
+		} else if (2*y == x or (21-2*y) == x){
+			c = 'X';
+		} else if (x==21){
+			c = 'D';
+		} else if ((x>=22 and x<=29 and x%2==1) and (y==1 or y==H-2)){
+			c = 'D';
+		} else if (x>29 and (2*y==(x-29+1) or (22-2*y)==(x-29+1))){
+			c = 'D';
+		} else if (x==W-3 and (y==5 or y==6)){
+			c = 'D';
+		} else {
+			c = ' ';
+		}
+		frame[y*W+x] = c;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	MemoryBuffer<char> frame(W*H);
+	auto frame_smem = frame.CreateSync(W*H);
+	CHECK;
+
+	Draw<<<dim3((W-1)/16+1,(H-1)/12+1), dim3(16,12)>>>(frame_smem.get_gpu_wo());
+	CHECK;
+
+	puts(frame_smem.get_cpu_ro());
+	CHECK;
+	return 0;
+}
